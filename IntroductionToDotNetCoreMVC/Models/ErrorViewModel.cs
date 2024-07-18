bbfe316d5ee858a4c2fc9df2093e358fa3e870ae
suresh.thiyagarajan <suresh.thiyagarajan@SYSLP1469>
2024-07-18 06:06:17 +0530

    Add project files.

---
+++ IntroductionToDotNetCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntroductionToDotNetCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
